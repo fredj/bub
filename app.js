dd681e64d13c35192f7a68befb6b2ef1579ce9f1
Tom MacWright <macwright@gmail.com>
2011-06-06 10:26:52 -0400

    Add timeAgo

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ Backbone.Model.prototype.sync = Backbone.Collection.prototype.sync = function(me
   }, options));
 };
 
+// From jed
+var timeAgo = function(a,b,c){for(b=[1e3,60,60,24],a=new Date-a,c=0;a>2*b[c];a/=b[c++]);return~~a+" "+"m0second0minute0hour0day".split(0)[c]+"s ago"};
+
 $(function() {
   var Organization = Backbone.Model.extend({
     initialize: function(options) {
